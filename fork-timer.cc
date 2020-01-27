f8f454ca6eb465c81ffbecb38f3cbbd3f684a116
Dylan Kreth <dylankreth@gmail.com>
2020-01-27 13:29:52 -0600

    initial .cc, .h, and exe

---
+++ fork-timer.cc
@@ -0,0 +1,22 @@
+/*
+ * Filename       fork-timer.cc
+ * Date           1/27/2020
+ * Author         Dylan Kreth
+ * Email          dylan.kreth@utdallas.edu
+ * Course         CS 4348.001 Spring 2020
+ * Copyright      2020, All Rights Reserved
+ */
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include "fork-timer.h"
+
+using namespace std;
+
+int main() {
+  cout << "Hello, World!" << endl;
+
+}
---
+++ fork-timer.h
@@ -0,0 +1,19 @@
+/*
+ * Filename       fork-timer.h
+ * Date           1/27/2020
+ * Author         Dylan Kreth
+ * Email          dylan.kreth@utdallas.edu
+ * Course         CE 438.001 Spring 2020
+ * Copyright      2020, All Rights Reserved
+ */
+
+
+
+#ifndef __FORKTIMER_H__
+#define __FORKTIMER_H__
+
+
+
+
+#endif /* __FORKTIMER_H__ */
+

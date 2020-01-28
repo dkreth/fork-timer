32b81c6fc04ab0af7ad2d74c1ee4b14e6a119ecc
Dylan Kreth <dylankreth@gmail.com>
2020-01-27 21:43:43 -0600

    print times, fork, and structure for pid handling

--- fork-timer.cc
+++ fork-timer.cc
@@ -7,7 +7,6 @@
  * Copyright      2020, All Rights Reserved
  */
 
-
 #include <iostream>
 //#include <fstream>
 //#include <string>
@@ -15,27 +14,51 @@
 
 #include <sys/time.h>
 #include <unistd.h>
+#include <stdio.h>
+
+#define DEBUG 0
 
 using namespace std;
 
-int main() {
+int main()
+{
   cout << "Hello, World!" << endl;
   struct timeval startTime;
   struct timeval endTime;
-  if(!gettimeofday(&startTime, NULL)){
-    cerr << "gettimeofday() returned -1. "
+  if (gettimeofday(&startTime, NULL))
+  {
+    cerr << "gettimeofday() returned -1. " << endl;
   }
   cout << "Start Time: " << startTime.tv_sec << endl;
   //call fork
-
-
-  //handle parent
-
-  //handle child
+  pid_t pid = fork();
 
   //get end time
+  if (gettimeofday(&endTime, NULL))
+  {
+    cerr << "gettimeofday() returned -1. " << endl;
+  }
 
-  //write results to a csv
+  struct timeval duration;
+  timersub(&endTime, &startTime, &duration);
+  if (DEBUG)
+  {
+    char output[50];
+    sprintf(output, "fork took %ld sec and %ld us", (long)duration.tv_sec, (long)duration.tv_usec);
+    cout << output << endl;
+  }
 
+  if (pid == 0)
+  { //child process
+    //TODO open pipe to parent and send
+  }
+  else
+  { //parent process
+    //TODO receive pipe from the child
+    // TODO open file
+    // TODO compare results
+    // TODO write results to csv
+  }
 
+  return 0;
 }
